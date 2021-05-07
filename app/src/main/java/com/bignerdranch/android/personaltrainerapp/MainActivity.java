9709b8779d6619691779e057f44b8da20e8fd893
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-05-07 16:31:51 -0400

    Created the design of the program & established the other activities

--- app/src/main/java/com/bignerdranch/android/personaltrainerapp/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/personaltrainerapp/MainActivity.java
@@ -2,13 +2,44 @@ package com.bignerdranch.android.personaltrainerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Declare fields for the widgets
+    private Button mAccessActivityButton, mCreateNewMemberButton, mReserveScheduleButton;
+    private Intent mIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAccessActivityButton = (Button)findViewById(R.id.access_activity_btn);
+        mCreateNewMemberButton = (Button)findViewById(R.id.create_new_member_btn);
+        mReserveScheduleButton = (Button)findViewById(R.id.reserve_a_schedule_btn);
+
+        mAccessActivityButton.setOnClickListener(this::onClick);
+        mCreateNewMemberButton.setOnClickListener(this::onClick);
+        mReserveScheduleButton.setOnClickListener(this::onClick);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.access_activity_btn) {
+            mIntent = new Intent(MainActivity.this, AccessActivity.class);
+
+            startActivity(mIntent);
+        }
+        else if(v.getId() == R.id.create_new_member_btn) {
+            mIntent = new Intent(MainActivity.this, CreateNewMemberActivity.class);
+
+            startActivity(mIntent);
+        }
+        else if(v.getId() == R.id.reserve_a_schedule_btn) {
+            mIntent = new Intent(MainActivity.this, ReserveScheduleActivity.class);
+        }
     }
 }
